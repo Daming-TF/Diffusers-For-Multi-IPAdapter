5a522f7551d96408cea66b43a2de98251a2bd2fc
Daming-TF <470680149@qq.com>
2024-01-20 17:23:45 +0800

    add experiment for faceid plus v2 with img2img pipe

--- my_script/debug.py
+++ my_script/debug.py
@@ -1,64 +1,8 @@
 
-import cv2
-from PIL import Image
-from insightface.app import FaceAnalysis
-import torch
-from diffusers import StableDiffusionPipeline, DDIMScheduler, AutoencoderKL
-import argparse
-
-import os
-
-
-def main():
-    source_dir = r'/mnt/nfs/file_server/public/mingjiahui/models'
-    base_model_path = fr'{source_dir}/Lykon--DreamShaper/'      # "SG161222/Realistic_Vision_V4.0_noVAE"
-    lora = f"{source_dir}/lora_1.5/Paper_Cutout.safetensors"
-    device = "cuda"
-
-    pipe = StableDiffusionPipeline.from_pretrained(
-        base_model_path,
-        torch_dtype=torch.float16,
-        feature_extractor=None,
-        safety_checker=None
-    ).to(device)
-    pipe.load_lora_weights(
-        os.path.dirname(lora), 
-        weight_name=os.path.basename(lora), 
-        adapter_name="style"
-        )
-    pipe.set_adapters(["style"], adapter_weights=[1.0])
-    pipe.delete_adapters("style")
-    pipe.load_lora_weights(
-        os.path.dirname(lora), 
-        weight_name=os.path.basename(lora), 
-        adapter_name="style"
-        )
-
-
-    generator = torch.Generator(device).manual_seed(42)
-    image = pipe(
-            prompt='1 chicken',
-            width=512, height=512,
-            num_inference_steps=20,
-            generator=generator,
-        ).images[0]
-    image.save('/home/mingjiahui/projects/IpAdapter/IP-Adapter/data/debug.jpg')
-
-
-if __name__ == '__main__':
-    main()
-
-"""
-from diffusers import DiffusionPipeline
-import torch
-
-pipeline = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16
-).to("cuda")
-pipeline.load_lora_weights("nerijs/pixel-art-xl", weight_name="pixel-art-xl.safetensors", adapter_name="pixel")
-pipeline.delete_adapters("pixel")
-pipeline.load_lora_weights("nerijs/pixel-art-xl", weight_name="pixel-art-xl.safetensors", adapter_name="pixel")
-generator = torch.Generator("cuda").manual_seed(42)
-image = pipeline('1 chicken').images[0]
-image.save('./debug.jpg')
-"""
+for i in range(10):
+    try:
+        assert i <5,f"i mus < 5, but i = {i}"
+        print(i)
+    except Exception as e:
+        print(f"{e}")
+        continue
---
+++ my_script/ui_v2_experiment/multi_ipadapter_diff_pipeline-txt2img.py
@@ -0,0 +1,211 @@
+import argparse
+from PIL import Image
+import cv2
+import json
+import os
+import sys
+import numpy as np
+from copy import deepcopy
+from tqdm import tqdm
+current_path = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(os.path.dirname(current_path)))
+from ui_v2 import load_model, data_prepare
+from my_script.util.util import FaceidAcquirer
+
+
+model_dir = r'/mnt/nfs/file_server/public/mingjiahui/models/h94--IP-Adapter/h94--IP-Adapter'
+image_encoder_paths = {
+    "ip-adapter_sdxl": f"{model_dir}/sdxl_models/image_encoder/",
+    "ip-adapter-faceid_sdxl": None,
+    "ip-adapter-plus_sdxl_vit-h": f"{model_dir}/models/image_encoder/",
+    "ip-adapter-plus-face_sdxl_vit-h": f"{model_dir}/models/image_encoder/",
+    "ip-adapter-faceid-plusv2_sdxl": f"{model_dir}/models/image_encoder/",
+}
+embeds_dir = "/mnt/nfs/file_server/public/lzx/repo/stable-diffusion-api/models/iplora/"
+LoRAs = {
+        '3dpixel2sd': f"{embeds_dir}/3DPixel2_SDXLIP_v0_embeddings.pt", 
+        'papercutoutsd': f"{embeds_dir}/PaperCutout_SDXLIP_v0_embeddings.pt", 
+        # '3dexaggeration_sd': f"{embeds_dir}/3DExaggeration_SDXLIP_v0_embeddings.pt", 
+        # 'graffitisplash_sd': f"{embeds_dir}/GraffitiSplash_SDXLIP_v0_embeddings.pt", 
+        # 'holographic_sd': f"{embeds_dir}/Holographic_SDXLIP_v0_embeddings.pt",
+        'colorfulrhythmsd': f'{embeds_dir}/Colorful_Rhythm_xl_1024_ip_image_embeddings.pt',
+        'joyfulcartoonsd': f'{embeds_dir}/Joyful_Cartoon_xl_1024_ip_image_embeddings.pt',
+    }
+# save_dir = r"./my_script/ui_v2_experiment/output/diff_pipe_comparison"
+save_dir = r"/mnt/nfs/file_server/public/mingjiahui/experiments/faceid/untreated_output/"
+os.makedirs(save_dir, exist_ok=True)
+
+from torchvision import transforms
+transform = transforms.Compose([
+    transforms.Resize(1024)
+])
+
+app = FaceidAcquirer()
+
+
+def pipe_process(image_paths, param_data, pipe_name, use_control):
+    print(f" \
+          base: \nbatch_size:{param_data['base']['batch_size']}\n\
+          Unit0: \nmodel:{param_data['Unit0']['model_id']}\tip scale:{param_data['Unit0']['multi_ip_scale']}\n\
+          Unit1: \nmodel:{param_data['Unit1']['model_id']}\tip scale:{param_data['Unit1']['ip_scale']}\n\
+          Unit2: \nmodel:{param_data['Unit2']['model_id']}\tip scale:{param_data['Unit2']['ip_scale']}\n\
+          ")
+    for lora_id, embeds_path in LoRAs.items():
+        for image_path in tqdm(image_paths):
+            # (1)prepare save path
+            dir_name = os.path.basename(os.path.dirname(image_path))
+            save_dir_ = os.path.join(save_dir, dir_name, lora_id, pipe_name)
+            os.makedirs(save_dir_, exist_ok=True)
+            try:
+                # (2)prepare input image
+                image = transform(Image.open(image_path).convert("RGB"))
+                for i in range(4):
+                    if not param_data[f'Unit{i}']['single_enable']:
+                        continue
+                    if 'plus-face' in param_data[f'Unit{i}']['model_id']:
+                        _, face_image = app.get_face_embeds(image)
+                        param_data[f'Unit{i}']['style_image'] = Image.fromarray(face_image[:, :, ::-1])
+                    else:
+                        param_data[f'Unit{i}']['style_image'] = image
+                if use_control:
+                    param_data['controlnet']['control_input'] = image
+
+                # (3)prepare prompt
+                suffix = os.path.basename(image_path).split('.')[1]
+                txt_path = image_path.replace(suffix, 'txt')
+                with open(txt_path, 'r')as f:
+                    prompts = f.readlines()
+                    assert len(prompts)==1, "txt file looks happening some error"
+                param_data['base']['prompt'] = prompts[0]
+                
+                # (4)check exists result
+                save_name = os.path.basename(image_path)
+                save_path = os.path.join(save_dir_, save_name)
+                # if os.path.exists(save_path):
+                #     continue
+
+                # (5)processing
+                result = None
+                # no style
+                # input_param = deepcopy(param_data)
+                # input_param['lora']['lora_id']=lora_id
+                # input_param['lora']['lora_scale']=0
+                # input_param['Unit0']['cache_path']=embeds_path
+                # input_param['Unit0']['multi_ip_scale']=0
+                # output = data_prepare(input_param, args)[0]
+                # result = np.array(output)
+
+                input_param = deepcopy(param_data)
+                input_param['lora']['lora_id']=lora_id
+                input_param['lora']['lora_scale']=1.0
+                input_param['Unit0']['cache_path']=embeds_path
+                input_param['Unit0']['multi_ip_scale']=0.8
+                output = data_prepare(input_param, args)[0]
+                result = cv2.vconcat([result, np.array(output)]) if result is not None \
+                    else np.array(output)
+                
+            except Exception as e:
+                print(f"********* {e}:==>{image_path} **************")
+                continue
+            Image.fromarray(result).save(save_path)
+            print(f"result has save in ==> {save_path}")
+
+
+def main(args):
+    # init
+    image_paths = [os.path.join(args.input_image_dir, name) for name in os.listdir(args.input_image_dir) \
+                   if name.split('.')[1] in ['jpg', 'png', 'webp']]
+    
+    # load model
+    with open(args.json_path, 'r')as f:
+        param_data = json.load(f)
+    ip_ckpt = []
+    image_encoder_path = []
+    for k, v in param_data.items(): 
+        if 'Unit' not in k:
+            continue
+        model_id = v['model_id']
+        if model_id == 'None' or model_id == '':
+            break
+        ip_ckpt.append(os.path.join(model_dir, 'sdxl_models', model_id))
+        print(f"model_id:{model_id}")
+        image_encoder_path.append(image_encoder_paths[model_id.split('.')[0]])
+    load_model(
+        base_model_path=args.base_model_path,
+        image_encoder_path=image_encoder_path,
+        ip_ckpt=ip_ckpt,
+        unet_load=True,
+    )
+
+    # # pipe0: only faceid(0.7)
+    # input_param = deepcopy(param_data)
+    # input_param['Unit1']['ip_scale']=0.7
+    # input_param['Unit1']['face_id_lora']=0.7
+    # pipe_process(image_paths, input_param, \
+    #              pipe_name="only_faceid_0.7", use_control=False)
+
+    assert param_data['Unit0']['model_id'] == "ip-adapter_sdxl.bin"
+    assert param_data['Unit1']['model_id'] == "ip-adapter-faceid-plusv2_sdxl.bin"
+    assert param_data['Unit2']['model_id'] == "ip-adapter-plus_sdxl_vit-h.bin"
+    # pipe1: only faceid plus v2(0.8)
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    pipe_process(image_paths, input_param, \
+                 pipe_name="only_faceid_plusv2_0.8", use_control=False)
+
+    # pipe2: faceid plus v2(0.8) + plus(0.3)
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    input_param['Unit2']['ip_scale']=0.3
+    pipe_process(image_paths, input_param, \
+                 pipe_name="faceid_plusV2_0.8--plus_0.3", use_control=False)
+
+    # pipe3: faceid plus v2(0.8) + controlnet 0.2
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    input_param['controlnet']['is_control']=True
+    input_param['controlnet']['control_weights']=0.2
+    pipe_process(image_paths, input_param, \
+                 pipe_name="faceid_plusV2_0.8--controlnet0.2", use_control=True)
+
+    # pipe4: faceid plus v2(0.8) + controlnet 0.2 + plus0.3
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    input_param['controlnet']['is_control']=True
+    input_param['controlnet']['control_weights']=0.2
+    input_param['Unit2']['ip_scale']=0.3
+    pipe_process(image_paths, input_param, \
+                 pipe_name="faceid_plusV2_0.8--plus_0.3--controlnet0.2", use_control=True)
+
+    # pipe5: faceid plus v2(0.8) + controlnet 0.5
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    input_param['controlnet']['is_control']=True
+    input_param['controlnet']['control_weights']=0.5
+    pipe_process(image_paths, input_param, \
+                 pipe_name="faceid_plusV2_0.8--controlnet0.5", use_control=True)
+
+    # pipe6: faceid plus v2(0.8) + controlnet 0.2 + plus0.3
+    input_param = deepcopy(param_data)
+    input_param['Unit1']['ip_scale']=0.8
+    input_param['Unit1']['face_id_lora']=0.8
+    input_param['controlnet']['is_control']=True
+    input_param['controlnet']['control_weights']=0.5
+    input_param['Unit2']['ip_scale']=0.3
+    pipe_process(image_paths, input_param, \
+                 pipe_name="faceid_plusV2_0.8--plus_0.3--controlnet0.5", use_control=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_model_path", type=str, default="/mnt/nfs/file_server/public/lipengxiang/sdxl_1_0/")
+    # parser.add_argument("--output", type=str, default=None)
+    parser.add_argument("--json_path", type=str, default=None, required=True)
+    parser.add_argument("--input_image_dir", type=str, required=True, help="image dir")
+    args = parser.parse_args()
+    main(args)
