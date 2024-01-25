d168fa0d2b414446b87b4d08d492e25216d530b0
Daming-TF <470680149@qq.com>
2024-01-25 11:31:13 +0800

    add portrait train code base sd15

---
+++ tutorial_train_faceid_costom_portrait.sh
@@ -0,0 +1,16 @@
+MODELDIR="/mnt/nfs/file_server/public/mingjiahui" 
+export CUDA_VISIBLE_DEVICES=6,7
+# \--config_file=default_config.yaml   # --num_processes 4 --multi_gpu --mixed_precision "fp16"
+accelerate launch --config_file=./default_config_multi_gpu.yaml --main_process_port 29435  tutorial_train_faceid_costom_portrait.py \
+    --pretrained_model_name_or_path="/mnt/nfs/file_server/public/mingjiahui/models/runwayml--stable-diffusion-v1-5" \
+    --data_json_file="/mnt/nfs/file_server/public/mingjiahui/experiments/faceid/train_json/traindata_V1_crop.json" \
+    --resolution=512 \
+    --train_batch_size=32 \
+    --dataloader_num_workers=2 \
+    --learning_rate=1e-04 \
+    --output_dir="$MODELDIR/experiments/faceid/finetune/20140125-sd15" \
+    --num_train_epochs=40 \
+    --enable_xformers_memory_efficient_attention \
+    --pretrained_ip_adapter_path="$MODELDIR/models/h94--IP-Adapter/h94--IP-Adapter/models/ip-adapter-faceid-portrait_sd15.bin" \
+    --save_steps=2000 \
+    --num_tokens=16
