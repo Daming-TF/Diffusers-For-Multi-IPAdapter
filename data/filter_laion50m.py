aa8e9f7f019a0dd9baf396027296f958651eaf25
Daming-TF <470680149@qq.com>
2024-04-03 18:16:22 +0800

    add face dect for laion50m

--- data/filter_laion50m.py
+++ data/filter_laion50m.py
@@ -7,7 +7,83 @@ import argparse
 import multiprocessing
 from requests.exceptions import Timeout
 import hashlib
+import torch
+from PIL import Image
+import cv2
+import numpy as np
 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def face_dect(img_paths):
+    save_dir_key = [
+        "data-50m-20240402",
+        "data-50m-20240402-embeds/antelopev2_embeds",
+    ]
+    from insightface.app import FaceAnalysis
+    from torchvision import transforms
+    app = FaceAnalysis(name='/home/mingjiahui/.insightface/models/antelopev2/', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+    app.prepare(ctx_id=0, det_size=(640, 640))
+    transfer = transforms.Resize(768)
+
+    for img_path in tqdm(img_paths):
+        meta_result = {}
+        meta_result.setdefault('landmark_2d_106', [])
+        meta_result.setdefault('landmark_3d_68', [])
+        meta_result.setdefault('kps', [])
+        meta_result.setdefault('pose', [])
+        meta_result.setdefault('sex', [])
+        meta_result.setdefault('age', [])
+        meta_result.setdefault('bbox', [])
+        meta_result.setdefault('embeds', [])
+        
+        suffix = os.path.basename(img_path).split('.')[1]
+        json_save_path = img_path.replace(save_dir_key[0], save_dir_key[1]).replace(suffix, 'json')
+        os.makedirs(os.path.dirname(json_save_path), exist_ok=True)
+        
+        if os.path.exists(json_save_path):
+            continue
+
+        try:
+            img = Image.open(img_path).convert("RGB")
+        except Exception as e:
+            print(e)
+            continue
+        ori_w, _ = img.size
+        img = transfer(img)
+        new_w, _ = img.size
+        ratio = ori_w / new_w
+        face_info = app.get(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
+
+        if len(face_info)==0:
+            logger.info(f"This img has not face info ==> {img_path}")
+            continue
+        face_info = sorted(face_info, key=lambda x:(x['bbox'][2]-x['bbox'][0])*(x['bbox'][3]-x['bbox'][1]))[-1]
+
+        meta_result['image_file'] = img_path
+        meta_result['landmark_2d_106'].append((face_info["landmark_2d_106"]*ratio).tolist())
+        meta_result['landmark_3d_68'].append((face_info["landmark_3d_68"]*ratio).tolist())
+        meta_result['kps'].append((face_info["kps"]*ratio).tolist())
+        meta_result['pose'].append((face_info["pose"]*ratio).tolist())
+        meta_result['sex'].append(face_info.sex)
+        meta_result['age'].append(face_info["age"])
+        meta_result['bbox'].append((face_info["bbox"]*ratio).tolist())
+
+        # face_emb = torch.from_numpy(face_info.normed_embedding).unsqueeze(0)
+        face_emb = torch.from_numpy(face_info.embedding).unsqueeze(0)
+        cache_save_dir = os.path.dirname(json_save_path)
+        cache_save_name = os.path.basename(json_save_path).replace('json', 'npy')
+        cache_path = os.path.join(cache_save_dir, cache_save_name)
+        os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+        np.save(cache_path, face_emb)
+        meta_result['embeds'].append(cache_path)
+
+        os.makedirs(os.path.dirname(json_save_path), exist_ok=True)
+        with open(json_save_path, 'w')as f:
+            json.dump(meta_result, f)  
+            
 
 def download_file(url, save_path, max_attempts=10, max_wait_time=5):
     for attempt in range(1, max_attempts + 1):
@@ -102,27 +178,27 @@ if __name__ == '__main__':
     parser.add_argument("--max_wait_time", type=int, default=5)
     args = parser.parse_args()
 
-    #### download 
-    parquet_list = []
-    data_dir = "/mnt/nfs/file_server/public/mingjiahui/data/Laion400m_face/laion_face_meta"
-    parquet_list = [os.path.join(data_dir, name) for name in os.listdir(data_dir) if name.endswith('.parquet')]
-    parquet_list = sorted(parquet_list)
-    data_index = 0
-    processors = []
-    chunk_num = len(parquet_list) // args.process_num
-    residue_num = len(parquet_list) % args.process_num
-    for i in range(args.process_num):
-        end_index = data_index+chunk_num+1 if i < residue_num else data_index+chunk_num
-        chunk_parquet = parquet_list[data_index:end_index]
-        data_index = end_index
-        processor = multiprocessing.Process(
-            target=download_process, 
-            args=(i, chunk_parquet, args))
-        processors.append(processor)
-        processor.start()
-    for processor in processors:
-        processor.join()
-    # **************************************
+    # #### download 
+    # parquet_list = []
+    # data_dir = "/mnt/nfs/file_server/public/mingjiahui/data/Laion400m_face/laion_face_meta"
+    # parquet_list = [os.path.join(data_dir, name) for name in os.listdir(data_dir) if name.endswith('.parquet')]
+    # parquet_list = sorted(parquet_list)
+    # data_index = 0
+    # processors = []
+    # chunk_num = len(parquet_list) // args.process_num
+    # residue_num = len(parquet_list) % args.process_num
+    # for i in range(args.process_num):
+    #     end_index = data_index+chunk_num+1 if i < residue_num else data_index+chunk_num
+    #     chunk_parquet = parquet_list[data_index:end_index]
+    #     data_index = end_index
+    #     processor = multiprocessing.Process(
+    #         target=download_process, 
+    #         args=(i, chunk_parquet, args))
+    #     processors.append(processor)
+    #     processor.start()
+    # for processor in processors:
+    #     processor.join()
+    # # **************************************
 
 
     # #### statistic laion data num:
@@ -141,3 +217,32 @@ if __name__ == '__main__':
     #     print(f"total: {total_num}")
     # # **************************************
 
+
+    #### face detect
+    source_path = "/mnt/nfs/file_server/public/mingjiahui/data/Laion400m_face/data-50m-20240402"
+    process_dirs = [os.path.join(source_path, name) for name in os.listdir(source_path)]
+    img_paths = []
+    for process_dir in tqdm(process_dirs):
+        img_dirs = [os.path.join(process_dir, name) for name in os.listdir(process_dir)]
+        for img_dir in img_dirs:
+            img_paths += [os.path.join(img_dir, name) for name in os.listdir(img_dir) if name.endswith('.jpg')]
+    print(f"total num:{len(img_paths)}")
+    exit(0)
+
+    processors = []
+    chunk_num = len(img_paths) // args.process_num
+    residue_num = len(img_paths) % args.process_num
+    data_index = 0
+    for i in range(args.process_num):
+        if i < residue_num:
+            chunk_data = img_paths[data_index:data_index+chunk_num+1]
+            data_index = data_index+chunk_num+1
+        else:
+            chunk_data = img_paths[data_index:data_index+chunk_num]
+            data_index = data_index+chunk_num
+        processor = multiprocessing.Process(target=face_dect, args=(chunk_data,))
+        processors.append(processor)
+        processor.start()
+    for processor in processors:
+        processor.join()
+    # *****************************************
