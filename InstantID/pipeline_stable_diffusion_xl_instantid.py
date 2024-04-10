ef4318dbfc1c374562df2ac9a4d8131eb2991347
Daming-TF <470680149@qq.com>
2024-04-10 10:43:39 +0800

    update instantid training code

--- InstantID/pipeline_stable_diffusion_xl_instantid.py
+++ InstantID/pipeline_stable_diffusion_xl_instantid.py
@@ -152,6 +152,52 @@ class StableDiffusionXLInstantIDPipeline(StableDiffusionXLControlNetPipeline):
                 self.enable_xformers_memory_efficient_attention()
             else:
                 raise ValueError("xformers is not available. Make sure it is installed correctly")
+
+    # jiahui's add for instantid training
+    def load_instantid(self, state_dict, num_tokens=16):
+        self.load_proj_model(state_dict, num_tokens=num_tokens)
+        unet = self.unet
+        attn_procs = {}
+        for name in unet.attn_processors.keys():
+            cross_attention_dim = None if name.endswith("attn1.processor") else unet.config.cross_attention_dim
+            if name.startswith("mid_block"):
+                hidden_size = unet.config.block_out_channels[-1]
+            elif name.startswith("up_blocks"):
+                block_id = int(name[len("up_blocks.")])
+                hidden_size = list(reversed(unet.config.block_out_channels))[block_id]
+            elif name.startswith("down_blocks"):
+                block_id = int(name[len("down_blocks.")])
+                hidden_size = unet.config.block_out_channels[block_id]
+            if cross_attention_dim is None:
+                attn_procs[name] = AttnProcessor().to(unet.device, dtype=unet.dtype)
+            else:
+                attn_procs[name] = IPAttnProcessor(hidden_size=hidden_size, 
+                                                   cross_attention_dim=cross_attention_dim, 
+                                                   num_tokens=num_tokens).to(unet.device, dtype=unet.dtype)
+        unet.set_attn_processor(attn_procs)
+        ip_layers = torch.nn.ModuleList(self.unet.attn_processors.values())
+        if 'ip_adapter' in state_dict:
+            ip_sd = state_dict['ip_adapter']
+        ip_layers.load_state_dict(ip_sd)
+
+    # jiahui's add for only control adapter training
+    def load_proj_model(self, state_dict, image_emb_dim=512, num_tokens=16):
+        image_proj_model = Resampler(
+            dim=1280,
+            depth=4,
+            dim_head=64,
+            heads=20,
+            num_queries=num_tokens,
+            embedding_dim=image_emb_dim,
+            output_dim=self.unet.config.cross_attention_dim,
+            ff_mult=4,
+        )
+        image_proj_model.eval()
+        self.image_proj_model = image_proj_model.to(self.device, dtype=self.dtype)
+        if 'image_proj' in state_dict:
+            proj_sd = state_dict["image_proj"]
+        self.image_proj_model.load_state_dict(proj_sd)
+        self.image_proj_model_in_features = image_emb_dim
     
     def load_ip_adapter_instantid(self, model_ckpt, image_emb_dim=512, num_tokens=16, scale=0.5):     
         self.set_image_proj_model(model_ckpt, image_emb_dim, num_tokens)
